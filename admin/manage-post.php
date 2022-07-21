b9539139e74324c55f431a83dbf002f46c0e22d2
asif khan <asiff080@gmail.com>
2022-07-22 03:49:00 +0500

    review manage post page

--- admin/manage-post.php
+++ admin/manage-post.php
@@ -8,7 +8,6 @@ if (strlen($_SESSION['alogin']) == 0) {
     if (isset($_REQUEST['post_id_ignore'])) {
         $post_id = intval($_GET['post_id_ignore']);
         $status = 1;
-        // $cancelby = 'a';
         $query = "DELETE FROM reports WHERE post_id=?";
         $stmt = $dbh->prepare($query);
         $stmt->execute(array($post_id));
@@ -17,7 +16,6 @@ if (strlen($_SESSION['alogin']) == 0) {
     if (isset($_REQUEST['post_id_delete'])) {
         $post_id = intval($_GET['post_id_delete']);
         $status = 1;
-        // $cancelby = 'a';
         $query = "DELETE FROM posts WHERE id=?";
         $stmt = $dbh->prepare($query);
         $stmt->execute(array($post_id));
@@ -35,7 +33,6 @@ if (strlen($_SESSION['alogin']) == 0) {
             addEventListener("load", function() {
                 setTimeout(hideURLbar, 0);
             }, false);
-
             function hideURLbar() {
                 window.scrollTo(0, 1);
             }
@@ -51,25 +48,19 @@ if (strlen($_SESSION['alogin']) == 0) {
         <script type="text/javascript">
             $(document).ready(function() {
                 $('#table').basictable();
-
                 $('#table-breakpoint').basictable({
                     breakpoint: 768
                 });
-
                 $('#table-swap-axis').basictable({
                     swapAxis: true
                 });
-
                 $('#table-force-off').basictable({
                     forceResponsive: false
                 });
-
                 $('#table-no-resize').basictable({
                     noResize: true
                 });
-
                 $('#table-two-axis').basictable();
-
                 $('#table-max-height').basictable({
                     tableWrapper: true
                 });
@@ -78,36 +69,8 @@ if (strlen($_SESSION['alogin']) == 0) {
         <link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css' />
         <link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+        <link rel="stylesheet" href="css/custom.css" type='text/css' />
         <style>
-            .errorWrap {
-                padding: 10px;
-                margin: 0 0 20px 0;
-                background: #fff;
-                border-left: 4px solid #dd3d36;
-                -webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-                box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-            }
-
-            .succWrap {
-                padding: 10px;
-                margin: 0 0 20px 0;
-                background: #fff;
-                border-left: 4px solid #5cb85c;
-                -webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-                box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-            }
-        </style>
-        <style>
-            #menu li a {
-                background-color: rgba(255, 255, 255, 1) !important;
-                color: #000 !important;
-            }
-
-            #menu li a:hover {
-                background-color: #4485AF !important;
-                color: #fff !important;
-            }
-
             #table thead tr th {
                 background-color: #4485AF !important;
             }
@@ -119,13 +82,11 @@ if (strlen($_SESSION['alogin']) == 0) {
             <!--/content-inner-->
             <div class="left-content">
                 <div class="mother-grid-inner">
-                    <!--header start here-->
                     <?php include('includes/header.php'); ?>
                     <div class="clearfix"> </div>
                 </div>
-                <!--heder end here-->
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.html">Home</a><i class="fa fa-angle-right"></i>Manage Reported Post</li>
+                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a><i class="fa fa-angle-right"></i>Manage Reported Post</li>
                 </ol>
                 <div class="agile-grids">
                     <!-- tables -->
@@ -149,14 +110,12 @@ if (strlen($_SESSION['alogin']) == 0) {
                                         $query = $dbh->prepare($sql);
                                         $query->execute();
                                         $results = $query->fetchAll(PDO::FETCH_OBJ);
-                                        //  echo '<pre>' . var_export($results, true) . '</pre>';
                                     } catch (\Throwable $th) {
                                         echo $th->getMessage();
                                     }
                                     $cnt = 1;
                                     if ($query->rowCount() > 0) {
-
-                                        foreach ($results as $result) {                ?>
+                                        foreach ($results as $result) { ?>
                                             <tr>
                                                 <td><?php echo htmlentities($cnt); ?></td>
                                                 <td><?php echo htmlentities($result->description); ?></td>
@@ -168,13 +127,10 @@ if (strlen($_SESSION['alogin']) == 0) {
                                             </tr>
                                     <?php $cnt = $cnt + 1;
                                         }
-                                    } else {
-                                        // echo '<pre>' . var_export($result, true) . '</pre>';
                                     } ?>
                                 </tbody>
                             </table>
                         </div>
-                        </table>
                     </div>
                     <!-- script-for sticky-nav -->
                     <script>
@@ -188,28 +144,15 @@ if (strlen($_SESSION['alogin']) == 0) {
                                     $(".header-main").removeClass("fixed");
                                 }
                             });
-
                         });
                     </script>
-                    <!-- /script-for sticky-nav -->
-                    <!--inner block start here-->
-                    <div class="inner-block">
-
-                    </div>
-                    <!--inner block end here-->
-                    <!--copy rights start here-->
-                    <?php include('includes/footer.php'); ?>
-                    <!--COPY rights end here-->
                 </div>
             </div>
-            <!--//content-inner-->
-            <!--/sidebar-menu-->
             <?php include('includes/sidebarmenu.php'); ?>
             <div class="clearfix"></div>
         </div>
         <script>
             var toggle = true;
-
             $(".sidebar-icon").click(function() {
                 if (toggle) {
                     $(".page-container").addClass("sidebar-collapsed").removeClass("sidebar-collapsed-back");
@@ -224,7 +167,6 @@ if (strlen($_SESSION['alogin']) == 0) {
                         });
                     }, 400);
                 }
-
                 toggle = !toggle;
             });
         </script>
@@ -234,7 +176,6 @@ if (strlen($_SESSION['alogin']) == 0) {
         <!-- Bootstrap Core JavaScript -->
         <script src="js/bootstrap.min.js"></script>
         <!-- /Bootstrap Core JavaScript -->
-
     </body>
 
     </html>
