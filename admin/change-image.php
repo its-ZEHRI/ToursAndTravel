dbe7b0b00a928be7d45fc880a3d9009354ec5af2
asif khan <asiff080@gmail.com>
2022-07-22 02:46:22 +0500

    create custome css file and shift the css to it from change password and change image file

--- admin/change-image.php
+++ admin/change-image.php
@@ -43,37 +43,11 @@ if (strlen($_SESSION['alogin']) == 0) {
 		<link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css' />
 		<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
-		<style>
-			.errorWrap {
-				padding: 10px;
-				margin: 0 0 20px 0;
-				background: #fff;
-				border-left: 4px solid #dd3d36;
-				-webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-				box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-			}
-			.succWrap {
-				padding: 10px;
-				margin: 0 0 20px 0;
-				background: #fff;
-				border-left: 4px solid #5cb85c;
-				-webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-				box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-			}
-		</style>
-		<style>
-			#menu li a {
-				background-color: rgba(255, 255, 255, 1) !important;
-				color: #000 !important;
-			}
-			#menu li a:hover {
-				background-color: #4485AF !important;
-				color: #fff !important;
-			}
-		</style>
+		<!-- custom css -->
+		<link rel="stylesheet" href="css/custom.css">
 	</head>
 	<body>
-		<div class="page-container">
+		<div class="page-container change-">
 			<!--/content-inner-->
 			<div class="left-content">
 				<div class="mother-grid-inner">
--- admin/change-password.php
+++ admin/change-password.php
@@ -28,10 +28,8 @@ if (strlen($_SESSION['alogin']) == 0) {
 		}
 	}
 ?>
-
 	<!DOCTYPE HTML>
 	<html>
-
 	<head>
 		<title>TMS | Admin Change Password</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -45,7 +43,6 @@ if (strlen($_SESSION['alogin']) == 0) {
 				window.scrollTo(0, 1);
 			}
 		</script>
-
 		<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
 		<link href="css/style.css" rel='stylesheet' type='text/css' />
 		<link rel="stylesheet" href="css/morris.css" type="text/css" />
@@ -54,6 +51,8 @@ if (strlen($_SESSION['alogin']) == 0) {
 		<link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css' />
 		<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+		<!-- Custom css -->
+		<link rel="stylesheet" href="css/custom.css">
 		<script type="text/javascript">
 			function valid() {
 				if (document.chngpwd.newpassword.value != document.chngpwd.confirmpassword.value) {
@@ -64,39 +63,7 @@ if (strlen($_SESSION['alogin']) == 0) {
 				return true;
 			}
 		</script>
-		<style>
-			.errorWrap {
-				padding: 10px;
-				margin: 0 0 20px 0;
-				background: #fff;
-				border-left: 4px solid #dd3d36;
-				-webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-				box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-			}
-
-			.succWrap {
-				padding: 10px;
-				margin: 0 0 20px 0;
-				background: #fff;
-				border-left: 4px solid #5cb85c;
-				-webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-				box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
-			}
-		</style>
-		<style>
-			#menu li a {
-				background-color: rgba(255, 255, 255, 1) !important;
-				color: #000 !important;
-			}
-
-			#menu li a:hover {
-				background-color: #4485AF !important;
-				color: #fff !important;
-			}
-		</style>
-
 	</head>
-
 	<body>
 		<div class="page-container">
 			<!--/content-inner-->
@@ -104,7 +71,6 @@ if (strlen($_SESSION['alogin']) == 0) {
 				<div class="mother-grid-inner">
 					<!--header start here-->
 					<?php include('includes/header.php'); ?>
-
 					<div class="clearfix"> </div>
 				</div>
 				<!--heder end here-->
@@ -113,14 +79,10 @@ if (strlen($_SESSION['alogin']) == 0) {
 				</ol>
 				<!--grid-->
 				<div class="grid-form">
-
 					<div class="grid-form1">
-
 						<?php if ($error) { ?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } else if ($msg) { ?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php } ?>
-
 						<div class="panel-body">
 							<form name="chngpwd" method="post" class="form-horizontal" onSubmit="return valid();">
-
 								<div class="form-group">
 									<label class="col-md-2 control-label">Current Password</label>
 									<div class="col-md-8">
@@ -132,7 +94,6 @@ if (strlen($_SESSION['alogin']) == 0) {
 										</div>
 									</div>
 								</div>
-
 								<div class="form-group">
 									<label class="col-md-2 control-label">New Password</label>
 									<div class="col-md-8">
@@ -144,7 +105,6 @@ if (strlen($_SESSION['alogin']) == 0) {
 										</div>
 									</div>
 								</div>
-
 								<div class="form-group">
 									<label class="col-md-2 control-label">Confirm Password</label>
 									<div class="col-md-8">
@@ -156,20 +116,17 @@ if (strlen($_SESSION['alogin']) == 0) {
 										</div>
 									</div>
 								</div>
-
 								<div class="col-sm-8 col-sm-offset-2">
 									<button type="submit" name="submit" class="btn-primary btn">Submit</button>
 									<button type="reset" class="btn-inverse btn">Reset</button>
 								</div>
+							</form>
 						</div>
-
-						</form>
 					</div>
 				</div>
 			</div>
 		</div>
 		<!--//grid-->
-
 		<!-- script-for sticky-nav -->
 		<script>
 			$(document).ready(function() {
@@ -182,28 +139,14 @@ if (strlen($_SESSION['alogin']) == 0) {
 						$(".header-main").removeClass("fixed");
 					}
 				});
-
 			});
 		</script>
-		<!-- /script-for sticky-nav -->
-		<!--inner block start here-->
-		<div class="inner-block">
-
-		</div>
-		<!--inner block end here-->
-		<!--copy rights start here-->
-		<?php include('includes/footer.php'); ?>
-		<!--COPY rights end here-->
-		</div>
-		</div>
 		<!--//content-inner-->
 		<!--/sidebar-menu-->
 		<?php include('includes/sidebarmenu.php'); ?>
 		<div class="clearfix"></div>
-		</div>
 		<script>
 			var toggle = true;
-
 			$(".sidebar-icon").click(function() {
 				if (toggle) {
 					$(".page-container").addClass("sidebar-collapsed").removeClass("sidebar-collapsed-back");
@@ -218,15 +161,12 @@ if (strlen($_SESSION['alogin']) == 0) {
 						});
 					}, 400);
 				}
-
 				toggle = !toggle;
 			});
 		</script>
 		<script src="js/jquery.nicescroll.js"></script>
 		<script src="js/scripts.js"></script>
 		<script src="js/bootstrap.min.js"></script>
-
 	</body>
-
 	</html>
 <?php } ?>
